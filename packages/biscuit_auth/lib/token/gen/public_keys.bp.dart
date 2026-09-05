481b887062ee57cc34883e87de3f18dba6e4043a
Piotr Orzechowski <piotr@orzechowski.tech>
2026-09-05 09:56:24 +0200

    Implement PublicKeys

--- packages/biscuit_auth/lib/token/gen/public_keys.bp.dart
+++ packages/biscuit_auth/lib/token/gen/public_keys.bp.dart
@@ -8,5 +8,13 @@ part of '../public_keys.dart';
 // **************************************************************************
 
 extension $PublicKeysExtension on PublicKeys {
+  bool _equals(Object other) =>
+      identical(this, other) ||
+      other is PublicKeys &&
+          const ListEquality<PublicKey>().equals(_keys, other._keys);
+
+  int get _hashCode =>
+      Object.hash('PublicKeys', const ListEquality<PublicKey>().hash(_keys));
+
   String _toString() => 'PublicKeys(_keys: $_keys)';
 }
--- packages/biscuit_auth/lib/token/public_keys.dart
+++ packages/biscuit_auth/lib/token/public_keys.dart
@@ -5,10 +5,11 @@ import 'package:biscuit_auth/crypto/crypto.dart';
 import 'package:biscuit_auth/error.dart';
 import 'package:biscuit_auth/src/boilerplate_gen_annotations.dart';
 import 'package:biscuit_auth/src/collection.dart';
+import 'package:collection/collection.dart';
 
 part 'gen/public_keys.bp.dart';
 
-@Boilerplate(equality: false)
+@boilerplate
 final class const PublicKeys._(final List<PublicKey> _keys) {
   factory([List<PublicKey>? keys]) => ._(keys ?? []);
 
@@ -17,6 +18,38 @@ final class const PublicKeys._(final List<PublicKey> _keys) {
   void extend(PublicKeys other) => disjoint(_keys, other._keys)
       ? _keys.addAll(other._keys)
       : throw const FormatError.publicKeyTableOverlap();
+
+  /// Inserts the key and returns its index in the table.
+  /// If the key is already in the table, returns its index.
+  int insert(PublicKey key) => switch (_keys.indexOf(key)) {
+    -1 => (_keys..add(key)).length - 1,
+    final i => i,
+  };
+
+  /// Inserts the key and returns its index in the table.
+  /// Throws [PublicKeyTableOverlapError] if the `key` is already in the table.
+  int insertOrThrow(PublicKey key) => switch (_keys.indexOf(key)) {
+    -1 => (_keys..add(key)).length - 1,
+    _ => throw const FormatError.publicKeyTableOverlap(),
+  };
+
+  int get(PublicKey key) => _keys.indexOf(key);
+
+  int currentOffset() => _keys.length;
+
+  PublicKeys splitAt(int offset) => .new(_keys.sublist(offset));
+
+  bool isDisjoint(PublicKeys other) => disjoint(_keys, other._keys);
+
+  PublicKey? getKey(int index) => _keys.elementAtOrNull(index);
+
+  List<PublicKey> toList() => _keys;
+
+  @override
+  bool operator ==(Object other) => _equals(other);
+
+  @override
+  int get hashCode => _hashCode;
 
   @override
   String toString() => _toString();
